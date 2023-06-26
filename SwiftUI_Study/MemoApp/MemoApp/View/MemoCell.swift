2094e5834ec0f4271b40b17267f449e85ef71233
Jinuk <jitoor610@gmail.com>
2023-06-26 17:32:09 +0900

    [0626] 메모앱 - 리스트 리팩토링

---
+++ SwiftUI_Study/MemoApp/MemoApp/View/MemoCell.swift
@@ -0,0 +1,30 @@
+//
+//  MemoCell.swift
+//  MemoApp
+//
+//  Created by 최진욱 on 2023/06/26.
+//
+
+import SwiftUI
+
+struct MemoCell: View {
+    @ObservedObject var memo: Memo
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(memo.content)
+                .font(.body)
+                .lineLimit(1)
+            
+            Text(memo.insertDate, style: .date)
+                .font(.caption)
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct MemoCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoCell(memo: Memo(content: "Test"))
+    }
+}
