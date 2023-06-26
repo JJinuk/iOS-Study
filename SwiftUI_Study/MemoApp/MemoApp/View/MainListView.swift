95269fe002c86eb3d7594ba2d2e876dff52a6366
Jinuk <jitoor610@gmail.com>
2023-06-26 18:08:44 +0900

    [0626] 메모앱 - 화면이동

--- SwiftUI_Study/MemoApp/MemoApp/View/MainListView.swift
+++ SwiftUI_Study/MemoApp/MemoApp/View/MainListView.swift
@@ -11,6 +11,7 @@ struct MainListView: View {
     // 하나의 데이터를 여러 뷰에 공유할 때 EnvironmentObject 속성 사용
     @EnvironmentObject var store: MemoStore
     
+    @State private var showComposer: Bool = false
     
     var body: some View {
         NavigationView {
@@ -19,6 +20,18 @@ struct MainListView: View {
             }
             .listStyle(.plain)
             .navigationTitle("내 메모")
+            .toolbar {
+                // plus 버튼을 누르면 아래 클로저 실행, showComposer가 true로 바뀜
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            // 바인딩한 속성이 true가 되면 아래 클로저 실행 클로저에서 리턴한 뷰를 모달방식으로 표시
+            .sheet(isPresented: $showComposer) {
+                ComposeView()
+            }
         }
     }
 }
