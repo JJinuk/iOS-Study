147c7de6dac2cae51013f84d4862709caf046824
Jinuk <jitoor610@gmail.com>
2023-06-26 17:11:54 +0900

    [0626] 메모앱 - UI구현

--- SwiftUI_Study/MemoApp/MemoApp/MainListView.swift
+++ SwiftUI_Study/MemoApp/MemoApp/MainListView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct MainListView: View {
+    // 하나의 데이터를 여러 뷰에 공유할 때 EnvironmentObject 속성 사용
+    @EnvironmentObject var store: MemoStore
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(store.list) { memo in
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1)
+                    
+                    Text(memo.insertDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 메모")
+        }
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
         MainListView()
+            .environmentObject(MemoStore())
     }
 }
--- SwiftUI_Study/MemoApp/MemoApp/MemoAppApp.swift
+++ SwiftUI_Study/MemoApp/MemoApp/MemoAppApp.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 @main
 struct MemoAppApp: App {
+    // 메모 저장소 속성으로 저장
+    @StateObject var store = MemoStore()
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
         }
     }
 }
